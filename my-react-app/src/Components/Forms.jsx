b599275cd102b8c48414ee0d3b6620cfefa76379
akshat <akshatbhatya@gmail.com>
2024-01-30 00:09:54 +0530

    configure form

--- my-react-app/src/Components/Forms.jsx
+++ my-react-app/src/Components/Forms.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 function Forms() {
     return (
         <div>
-            <form action="" className='mx-auto w-6/12 flex justify-center items-center gap-3 flex-col bg-black py-20 rounded-md'>
-                <input type="email" placeholder='Enter your Email' className='capitalize text-black' />
-                <input type="email" placeholder='password' className='capitalize text-black' />
-                <button type="submit" className='bg-red-600 text-white font-medium py-2 px-4 rounded-sm'>submit</button>
+            <form action="" className='mx-auto w-[500px] flex justify-center items-center gap-3 flex-col bg-black py-10 rounded-md'>
+
+                <h1 className='text-white font-extrabold'>Sign Up</h1>
+                <hr className='border border-white w-[200px]' />
+                <input type="email" placeholder='Enter your Email' className='capitalize w-auto text-black p-2' />
+                <input type="email" placeholder='password' className='capitalize w-auto text-black p-2' />
+                <button type="submit" className='bg-red-600 text-white font-medium py-2 px-4 rounded-sm capitalize '>submit</button>
             </form>
         </div>
     )
