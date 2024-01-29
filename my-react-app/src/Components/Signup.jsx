183cafbffc90a78afb3a7d54477f9d9e18a6e231
akshat <akshatbhatya@gmail.com>
2024-01-30 00:17:40 +0530

    add some tailwind

--- my-react-app/src/Components/Signup.jsx
+++ my-react-app/src/Components/Signup.jsx
@@ -5,11 +5,11 @@ function Signup() {
   return (
     <>
 
-    <div>
-    <img src="https://assets.nflxext.com/ffe/siteui/vlv3/32c47234-8398-4a4f-a6b5-6803881d38bf/eed3a573-8db7-47ca-a2ce-b511e0350439/IN-en-20240122-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="background" />
+    <div className='bg-[#0000000f]'>
+    <img src="https://assets.nflxext.com/ffe/siteui/vlv3/32c47234-8398-4a4f-a6b5-6803881d38bf/eed3a573-8db7-47ca-a2ce-b511e0350439/IN-en-20240122-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="background" className='w-[100vw] h-[100vh]' />
 
     </div>
-    <div className=''>
+    <div className='absolute top-[200px] left-[500px]'>
         
 
      <Forms/>
