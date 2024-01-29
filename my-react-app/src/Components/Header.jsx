316794f4a7f8f41a402269a1449b8330647fa805
akshat <akshatbhatya@gmail.com>
2024-01-29 23:43:42 +0530

    changes in header

--- my-react-app/src/Components/Header.jsx
+++ my-react-app/src/Components/Header.jsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
     return (
         <div className='flex justify-between w-[1200px] mx-auto absolute mt-10 left-[180px]'>
-            <img src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" className="w-[160px] h-[70px]" alt="Logo" />
+            <NavLink to={"/"}><img src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" className="w-[160px] h-[70px]" alt="Logo" /></NavLink>
             <nav>
                 <NavLink to={"/signup"}><button className="list-none bg-red-600 px-3 py-2 text-white rounded-md">sign up</button></NavLink>
             </nav>
--- my-react-app/src/Components/Signup.jsx
+++ my-react-app/src/Components/Signup.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Forms from './Forms'
 
 function Signup() {
   return (
     <div>
-      sign up page
+       <img src="https://assets.nflxext.com/ffe/siteui/vlv3/32c47234-8398-4a4f-a6b5-6803881d38bf/eed3a573-8db7-47ca-a2ce-b511e0350439/IN-en-20240122-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="background" />
+     <Forms/>
     </div>
   )
 }
