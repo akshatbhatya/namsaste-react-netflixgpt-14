1a92ba8dcba273a59a002b1d7a342a08a0dbd9b7
akshat <akshatbhatya@gmail.com>
2024-01-30 00:02:16 +0530

    add form

--- my-react-app/src/Components/Forms.jsx
+++ my-react-app/src/Components/Forms.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 function Forms() {
-  return (
-    <div>
-       
-    </div>
-  )
+    return (
+        <div>
+            <form action="" className='mx-auto w-6/12 flex justify-center items-center gap-3 flex-col bg-black py-20 rounded-md'>
+                <input type="email" placeholder='Enter your Email' className='capitalize text-black' />
+                <input type="email" placeholder='password' className='capitalize text-black' />
+                <button type="submit" className='bg-red-600 text-white font-medium py-2 px-4 rounded-sm'>submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default Forms
--- my-react-app/src/Components/Signup.jsx
+++ my-react-app/src/Components/Signup.jsx
@@ -3,10 +3,18 @@ import Forms from './Forms'
 
 function Signup() {
   return (
+    <>
+
     <div>
-       <img src="https://assets.nflxext.com/ffe/siteui/vlv3/32c47234-8398-4a4f-a6b5-6803881d38bf/eed3a573-8db7-47ca-a2ce-b511e0350439/IN-en-20240122-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="background" />
+    <img src="https://assets.nflxext.com/ffe/siteui/vlv3/32c47234-8398-4a4f-a6b5-6803881d38bf/eed3a573-8db7-47ca-a2ce-b511e0350439/IN-en-20240122-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="background" />
+
+    </div>
+    <div className=''>
+        
+
      <Forms/>
     </div>
+    </>
   )
 }
 
