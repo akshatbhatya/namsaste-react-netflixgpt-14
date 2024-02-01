f3ba58e2e19cb7bfa3f13f1910411affc55b579b
akshat <akshatbhatya@gmail.com>
2024-02-01 19:20:45 +0530

    add optional chaning

--- my-react-app/src/Components/VideoBackground.jsx
+++ my-react-app/src/Components/VideoBackground.jsx
@@ -4,16 +4,16 @@ import useMoviesTrailer from '../Hooks/useMoviesTrailer';
 function VideoBackground({ videoId }) {
 
     useMoviesTrailer(videoId)
-    let trailerData = useSelector(state => state.movie.movieTrailer)
+    let trailerData = useSelector(state => state.movie?.movieTrailer)
     if (!trailerData) return
-    
+
     return (
         <>
 
             <iframe
                 width="560"
                 height="315"
-                src={"https://www.youtube.com/embed/UGc5Tzz19UY?si=" + trailerData.key} title="YouTube video player"
+                src={"https://www.youtube.com/embed/UGc5Tzz19UY?si=" + trailerData?.key} title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowFullScreen>
