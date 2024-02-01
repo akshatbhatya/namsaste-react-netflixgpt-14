3e6a5d5f668d9f231ff23e1d66e73f518bd2d56d
akshat <akshatbhatya@gmail.com>
2024-02-01 11:24:47 +0530

    call api

--- my-react-app/src/Components/Browse.jsx
+++ my-react-app/src/Components/Browse.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { options } from '../Constants/Constant';
+
+
 
 const Browse = () => {
+
+  let [movie,setMovie]=useState([])
+
+  let fetchData=async()=>{
+
+    try{
+      let data=await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options);
+      let response=await data.json()
+      console.log(response.results);
+      setMovie(response)
+
+    }catch(error){
+      console.log(error);
+
+    }
+
+  }
+  useEffect(()=>{
+    fetchData()
+
+  },[])
   return (
-    <div>Browse</div>
+    <></>
   )
 }
 
---
+++ my-react-app/src/Constants/Constant.js
@@ -0,0 +1,10 @@
+export let profile_Avtar=""
+
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODM2NjUzZDE0Y2JiOWY0NzY3NjkxYTM5YmE3MDYyMSIsInN1YiI6IjY1YmE4NDY3ZGE5ZWYyMDE3Y2E4MGVjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0LPY4IybLTYegCMgCSGuim56EfI6prnrA2a3Rp8e5A0'
+    }
+};
+
