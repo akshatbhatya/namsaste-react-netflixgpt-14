d234446d0c7f3df304d3a5dd6385714af4a13835
akshat <akshatbhatya@gmail.com>
2024-02-01 12:42:38 +0530

    customize hook

--- my-react-app/src/Hooks/useFetchMovies.js
+++ my-react-app/src/Hooks/useFetchMovies.js
@@ -1,6 +1,8 @@
 import { useDispatch } from "react-redux";
 import { options } from "../Constants/Constant";
 import { useEffect } from "react";
+import { addMovies } from "../Store/MovieSlice";
+
 
 
 function GetData(){
@@ -18,7 +20,6 @@ function GetData(){
     try{
       let data=await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options);
       let response=await data.json()
-      console.log(response.results);
       dispatch(addMovies(response.results)
       )
       
