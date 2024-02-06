3337e8f57b2a0ad633cc9114ef5921ad94068fb8
akshat <akshatbhatya@gmail.com>
2024-02-06 13:48:51 +0530

    add new parent to card

--- my-react-app/src/Components/MovieList.jsx
+++ my-react-app/src/Components/MovieList.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import MovieCard from './MovieCard'
 
-function MovieList({Movies}) {
+function MovieList({ Movies }) {
   return (
-    <div className='mx-5 mt-7 bg-black'>
+    <div className=' bg-black'>
 
-    <h2 className='mb-7 text-white'>Now Playing</h2>
-   
-   <div className='flex flex-wrap gap-5 justify-center'>
+      <div className='mx-5 mt-7'>
 
-   {
-    Movies.map((item,i)=>{
-      return <MovieCard key={i} imagePath={item}/>
-    })
-   }
-    
-   </div>
-   
-   
-      
+        <h2 className='mb-7 text-white'>Now Playing</h2>
+
+        <div className='flex flex-wrap gap-5 justify-center'>
+
+          {
+            Movies.map((item, i) => {
+              return <MovieCard key={i} imagePath={item} />
+            })
+          }
+
+        </div>
+
+
+
+      </div>
     </div>
   )
 }
